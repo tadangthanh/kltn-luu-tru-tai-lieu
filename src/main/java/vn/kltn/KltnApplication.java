2551b5f568386ced8694dbaeb8154b4a23bd970f
=tadangthanh <tathanh.mycv@gmail.com>
2025-02-12 14:25:32 +0700

    Remove secret from .env

--- src/main/java/vn/kltn/KltnApplication.java
+++ src/main/java/vn/kltn/KltnApplication.java
@@ -1,5 +1,6 @@
 package vn.kltn;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KltnApplication {
 
 	public static void main(String[] args) {
+		// Load .env variables into environment
+		Dotenv dotenv = Dotenv.configure().load();
+		System.setProperty("AZURE_STORAGE_CONNECTION_STRING", dotenv.get("AZURE_STORAGE_CONNECTION_STRING"));
 		SpringApplication.run(KltnApplication.class, args);
 	}
 
