e88b29f799468df9785bf9324cfd28d398e5d9e9
=tadangthanh <tathanh.mycv@gmail.com>
2025-02-15 12:46:43 +0700

    update

--- src/main/java/vn/kltn/security/SecurityConfig.java
+++ src/main/java/vn/kltn/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
