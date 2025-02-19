251be30a9c41132c88638486cca7ae696f4abb6e
=tadangthanh <tathanh.mycv@gmail.com>
2025-02-19 20:24:16 +0700

    comment feature

--- src/main/java/vn/kltn/service/IRepoService.java
+++ src/main/java/vn/kltn/service/IRepoService.java
@@ -11,9 +11,15 @@ public interface IRepoService {
 
     void deleteRepository(Long id);
 
-    RepoResponseDto addMemberToRepository(Long repoId,Long userId, Set<RepoPermission> permissionRequest);
+    RepoResponseDto addMemberToRepository(Long repoId, Long userId, Set<RepoPermission> permissionRequest);
 
-    void removeMemberFromRepository(Long repoId,Long memberId);
+    void removeMemberFromRepository(Long repoId, Long memberId);
 
     RepoResponseDto updatePermissionMember(Long repoId, Long memberId, Set<RepoPermission> permissionRequest);
+//    Quản lý repo: Cập nhật repo, chuyển quyền sở hữu.
+//            ✅ Quản lý thành viên: Xem danh sách, kiểm tra quyền, mời qua email.
+//✅ Quản lý dung lượng: Kiểm tra dung lượng sử dụng, đặt giới hạn.
+//            ✅ Lịch sử hoạt động: Theo dõi thay đổi, audit logs.
+//✅ Tìm kiếm và lọc: Dễ dàng tìm và quản lý repo.
+//✅ Sao lưu và phục hồi: Đảm bảo an toàn dữ liệu.
 }
